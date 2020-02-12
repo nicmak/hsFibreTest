c6ee6ec6c5aa9a4de91c99dbdef2a16081bddaa7
nicmak <nicholas123mak@gmail.com>
2020-02-12 09:51:56 -0800

    testing if window is available

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      console.log('windw', window)
+    }
+  })
+  return (
+    <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -16,6 +22,7 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+  )
+}
 
 export default IndexPage
